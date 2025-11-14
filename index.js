a5dcc30ec44a9a004b53037d6054fa8078179fc6
gitacc-hub <github2001acc@gmail.com>
2025-11-14 18:25:00 +0300

    Added my first HTML and Javascript file.

---
+++ index.js
@@ -0,0 +1,34 @@
+/*
+let username;
+document.getElementById("mySubmit").onclick=function(){
+    username=document.getElementById("username").value;
+    document.getElementById("myH1").textContent=`6 7 ${username}` ;
+}
+*/
+
+// Prompting user for age and incrementing it
+
+
+//User the Number() function to convert string to number
+/*
+let age=prompt("Enter your age: ");
+age=Number(age);
+age+=1;
+console.log("Your age is "+age)12
+*/
+
+//How to check the type of a variable
+/*let age=prompt("Enter your age: ");
+console.log(typeof age);*/
+
+let a="Simba";
+let b="Simba";
+let c="";
+
+a=Number(a);
+b=String(b);
+c=Boolean(c);
+
+console.log(a,typeof a);
+console.log(b,typeof b);
+console.log(c,typeof c);
